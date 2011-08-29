56e3982916ba6d73a1773e804c021f14e4a368aa
martian-a <coder@bluegumtree.com>
2011-08-29 04:23:29 +0200

    Tidy up.

--- bluegumtree.code.java.goodreads.collector/src/uk/co/bluegumtree/code/java/goodreads/collector/model/GoodreadsApiConnectionSettings.java
+++ bluegumtree.code.java.goodreads.collector/src/uk/co/bluegumtree/code/java/goodreads/collector/model/GoodreadsApiConnectionSettings.java
@@ -3,8 +3,8 @@ package uk.co.bluegumtree.code.java.goodreads.collector.model;
 public class GoodreadsApiConnectionSettings {
 
 	private static final String LOCATION = "http://www.goodreads.com/";
-	private static final String KEY = "LNWVHb7t6EER4dReUzfZg";
-	private static final String SECRET = "DI2sjBbdRzoEoWkrQTz48QQlHY47OCTQIoALP0RwkTU";
+	private static final String KEY = "ADD YOUR OWN";
+	private static final String SECRET = "ADD YOUR OWN";
 
 	public static String getKey() {
 		return KEY;
